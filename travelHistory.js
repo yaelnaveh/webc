6f3432d3b6add6f404f0bef08dd000880e97eadd
estiley30 <estiley@post.bgu.ac.il>
2024-02-20 19:01:13 +0200

    last changes

--- travelHistory.js
+++ travelHistory.js
@@ -1,11 +1,22 @@
 
-// Add an event listener to the "Show Travel" button
-document.getElementById("showTravelButton").addEventListener("click", showChosenRide);
+document.getElementById("showTravelButton").addEventListener("click", checkRadio);
+let systemMessageHistPage= document.getElementById("systemMessageHistPage");
+// const radio= document.querySelectorAll('selectRow');
 
-function showChosenRide() {
-    let radioBtns= document.querySelectorAll("input[name='selectRow']");
-    let result;
-    radioBtns.forEach(radioBtn => {
-        radioBtn.addEventListener("change", finds)
-    })
+function displayError(){
+systemMessageHistPage.classList.add("open-error");
+    setTimeout(function () {
+        systemMessageHistPage.classList.remove("open-error");
+    }, 4000);
+}
+function checkRadio(){
+    let selectedOption = document.querySelector('input[name="selectRow"]:checked');
+    if (selectedOption) {
+        console.log("Selected option found");
+        selectedOption.checked = false;
+        window.location.href = "showTravel.html"
+    } else {
+        console.log("No selected option");
+        displayError()
+    }
 }
